abe68f1d1898148c5868d89eb9cf8a1dbe4a4dc6
Christine Chen <zhzhnaicha@gmail.com>
2026-01-07 06:58:34 -0500

    add networking.c

---
+++ networking.c
@@ -0,0 +1,88 @@
+#include "networking.h"
+
+void err(int i, char*message){
+  if(i < 0){
+	  printf("Error: %s - %s\n",message, strerror(errno));
+  	exit(1);
+  }
+}
+
+/*Create and bind a socket.
+* Place the socket in a listening state.
+* returns the socket descriptor
+*/
+int server_setup() {
+  //setup structs for getaddrinfo
+  struct addrinfo * hints, * results;
+  hints = calloc(1,sizeof(struct addrinfo));
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM;
+  hints->ai_flags = AI_PASSIVE;
+
+  err(getaddrinfo(NULL, PORT, hints, &results), "getaddrinfo error");
+
+  //create the socket
+  int clientd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+  err(clientd, "socket error");
+  
+  //this code should get around the address in use error
+  int yes = 1;
+  int sockOpt = setsockopt(clientd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+  err(sockOpt,"sockopt error");
+
+  //bind the socket to address and PORT
+  err(bind(clientd, results->ai_addr, results->ai_addrlen), "bind error");
+
+  //set socket to listen state
+  err(listen(clientd, 10), "listen error");
+
+  //free the structs used by getaddrinfo
+  free(hints);
+  freeaddrinfo(results);
+
+  return clientd;
+}
+
+/*Accept a connection from a client
+ *return the socket descriptor for the new socket connected to the client
+ *blocks until connection is made.
+ */
+int server_tcp_handshake(int listen_socket){
+    int client_socket;
+
+    //accept() the client connection
+    socklen_t sock_size;
+    struct sockaddr_storage client_address;
+    sock_size = sizeof(client_address);
+    client_socket = accept(listen_socket,(struct sockaddr *)&client_address, &sock_size);
+    err(client_socket, "accept error");
+
+    return client_socket;
+}
+
+
+/*Connect to the server
+ *return the to_server socket descriptor
+ *blocks until connection is made.*/
+int client_tcp_handshake(char * server_address) {
+
+  //setup structs for getaddrinfo
+  struct addrinfo * hints, * results;
+  hints = calloc(1,sizeof(struct addrinfo));
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM;
+
+  err(getaddrinfo(server_address, PORT, hints, &results), "getaddrinfo error");
+
+  //create the socket
+  int serverd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+  err(serverd, "socket error");
+
+  //connect() to the server
+  err(connect(serverd, results->ai_addr, results->ai_addrlen), "connect error");
+
+  free(hints);
+  freeaddrinfo(results);
+
+  return serverd;
+}
