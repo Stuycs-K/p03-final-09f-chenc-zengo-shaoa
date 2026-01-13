d92bb1303759c05a70c7cc11874976b9ffec9f2e
Christine Chen <zhzhnaicha@gmail.com>
2026-01-13 14:31:03 -0500

    restore message.h

--- message.h
+++ message.h
@@ -1,15 +1,16 @@
-#ifndef message_H
-#define message_H
+#ifndef MESSAGE_H
+#define MESSAGE_H
 
 struct message{
 	char *username;
 	char *message;
 	struct message *prev;
-}
+};
 
 struct message *makeMessage(char *username, char *message);
-
+void freeMsg(struct message *m);
 
 
 
 #endif
+
