ec354df661f54dabdc125c9dc2ef49cb27123057
Owen Zeng <owenzeng1002@gmail.com>
2026-01-14 01:48:35 -0500

    restored resize functionality

--- client.c
+++ client.c
@@ -6,94 +6,91 @@ char chat[MAX_MSG][MAX_MSG_LEN];
 int chat_count = 0;
 
 static void sighandler(int signo) { // crtl-c handler
-	end_ui();
-	close(server_socket);
-	exit(0);
+  end_ui();
+  close(server_socket);
+  exit(0);
 }
 
+void selectLogic(int server_socket, fd_set *read_fds, char *buff) {
+  int bytes = 0;
 
-void selectLogic(int server_socket, fd_set *read_fds, char *buff){
-	int bytes = 0;
+  FD_ZERO(read_fds);
+  FD_SET(server_socket, read_fds);
 
-	FD_ZERO(read_fds);
-	FD_SET(server_socket,read_fds);
+  struct timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = 10000; // 10ms
 
-	struct timeval tv;
-	tv.tv_sec = 0;
-	tv.tv_usec = 10000; // 10ms
+  if (select(server_socket + 1, read_fds, NULL, NULL, &tv) == -1) {
+    if (errno == EINTR)
+      return;
+    perror("select error");
+    end_ui();
+    close(server_socket);
+    return;
+  }
+  // socket (receive msg)
+  if (FD_ISSET(server_socket, read_fds)) {
+    bytes = read(server_socket, buff, sizeof(buff));
+    if (bytes <= 0) {
+      end_ui();
+      close(server_socket);
+      fprintf(stderr, "Socket Read Error (Bytes read: %d)", bytes);
+      exit(0);
+    }
+    if (strchr(buff, '\n')) {
+      buff[bytes] = '\0';
+      if (chat_count < MAX_MSG && bytes > 0) {
+        // memset(chat[chat_count], 0, MAX_MSG_LEN-1);
+        // strncpy(chat[chat_count], buff, MAX_MSG_LEN - 1);
+        strcat(chat[chat_count], buff);
 
-	if (select(server_socket+1, read_fds, NULL, NULL, &tv) == -1) {
-		perror("select error");
-		end_ui();
-		close(server_socket);
-		return;
-	}
-	// socket (receive msg)
-	if (FD_ISSET(server_socket, read_fds)) {
-		bytes = read(server_socket, buff, sizeof(buff));
-		if (bytes <= 0) {
-			end_ui();
-			close(server_socket);
-			fprintf(stderr, "Socket Read Error (Bytes read: %d)", bytes);
-			exit(0);
-		}
-		if(strchr(buff, '\n')){
-			buff[bytes] = '\0';
-			if (chat_count < MAX_MSG && bytes > 0) {
-				//memset(chat[chat_count], 0, MAX_MSG_LEN-1);
-				//strncpy(chat[chat_count], buff, MAX_MSG_LEN - 1);
-				strcat(chat[chat_count], buff);
-
-				chat[chat_count][MAX_MSG_LEN - 1] = '\0';
-				chat_count++;
-				memset(chat[chat_count], 0, MAX_MSG_LEN-1);
-			}
-		}
-		else if (bytes > 0) {
-			strcat(chat[chat_count], buff);
-
-		}
-	}
+        chat[chat_count][MAX_MSG_LEN - 1] = '\0';
+        chat_count++;
+        memset(chat[chat_count], 0, MAX_MSG_LEN - 1);
+      }
+    } else if (bytes > 0) {
+      strcat(chat[chat_count], buff);
+    }
+  }
 }
 
+void clientLogic(int server_socket) {
+  fd_set read_fds;
+  char buff[BUFFER_SIZE];
+  memset(buff, 0, BUFFER_SIZE);
 
-void clientLogic(int server_socket){
-	fd_set read_fds;
-	char buff[BUFFER_SIZE];
-	memset(buff, 0, BUFFER_SIZE);
+  // buff for typed input
+  char input[BUFFER_SIZE];
+  int input_len = 0;
+  memset(input, 0, BUFFER_SIZE);
+  int cursor = 0;
 
-	// buff for typed input
-	char input[BUFFER_SIZE];
-	int input_len = 0;
-	memset(input, 0, BUFFER_SIZE);
-	int cursor = 0;
+  char user_name[32];
 
-	char user_name[32];
+  // Naming the client
+  while (!setup_name_page(input, &input_len, user_name, server_socket)) {
+  }
 
-	// Naming the client
-	while(!setup_name_page(input, &input_len, user_name, server_socket)){}
+  while (1) {
+    // INPUT DETECT: handles all inputs; see ui.c for implementation
+    input_detect(input, &input_len, &cursor, server_socket, 0, user_name);
 
-	while(1) {
-		// INPUT DETECT: handles all inputs; see ui.c for implementation
-		input_detect(input, &input_len, &cursor, server_socket, 0, user_name);
+    selectLogic(server_socket, &read_fds, buff);
 
-		selectLogic(server_socket, &read_fds, buff);
-
-		setup_ui(input, chat, chat_count, user_name);
-
-	}
-
+    setup_ui(input, chat, chat_count, user_name);
+  }
 }
 
-int main(int argc, char *argv[] ) {
-	signal(SIGINT, sighandler);
-	char* IP = "127.0.0.1";
-	if(argc >= 2){
-		IP=argv[1];
-	}
-	server_socket = client_tcp_handshake(IP); // connect to server
-	//for(int i = 0; i < MAX_MSG; i++) what is this
-	init_ui(); // initialize ncurses
-	printf("client connected.\n");
-	clientLogic(server_socket);
+int main(int argc, char *argv[]) {
+  signal(SIGINT, sighandler);
+  char *IP = "127.0.0.1";
+  if (argc >= 2) {
+    IP = argv[1];
+  }
+  server_socket = client_tcp_handshake(IP); // connect to server
+  // for(int i = 0; i < MAX_MSG; i++) what is this
+  init_ui(); // initialize ncurses
+  printf("client connected.\n");
+  clientLogic(server_socket);
 }
